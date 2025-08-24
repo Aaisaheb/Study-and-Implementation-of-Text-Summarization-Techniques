552826e8725441299de43e2b2ced06d781e7227d
Vinod Chaudhari <vinodchaudhari0604@gmail.com>
2025-08-24 11:50:28 +0530

    First commit - Text Summarization Project

---
+++ src/test/java/com/example/textsummarized/textsummary/TextsummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textsummarized.textsummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextsummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
